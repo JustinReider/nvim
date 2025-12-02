28d6e3498da31302a596283899cd3f31977bb33e
Jus10eR <84348179+Jus10eR@users.noreply.github.com>
2025-12-02 01:45:23 +0100

    revert faulty changes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -24,37 +24,32 @@ return {
 				},
 				-- keymaps
 				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-
-					-- Auswahl bestätigen
+					["<C-Space>"] = function()
+						if cmp.visible() then
+							cmp.abort()
+						else
+							cmp.complete()
+						end
+					end,
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-					-- Navigation
-					["<Tab>"] = cmp.mapping(function(fallback)
+					["<Tab>"] = function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
-						elseif vim.snippet and vim.snippet.expandable() then
-							vim.snippet.expand()
-						elseif vim.snippet and vim.snippet.active() then
-							vim.snippet.jump(1)
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
 						else
 							fallback()
 						end
-					end, { "i", "s"}),
-
-					["<S-Tab>"] = cmp.mapping(function(fallback)
+					end,
+					["<S-Tab>"] = function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
-						elseif vim.snippet and vim.snippet.active() then
-							vim.snippet.jump(-1)
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
 						else
 							fallback()
 						end
-					end, { "i", "s"}),
-
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					end,
 				}),
 				sources = {
 					{ name = "nvim_lsp" },
