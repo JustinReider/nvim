02027c217db862f2133a6e619dbe4b2e5d36c09e
Justin <reider980@gmail.com>
2026-04-22 20:16:11 +0200

    add molten

--- lua/plugins/jupytext.lua
+++ lua/plugins/jupytext.lua
@@ -1,6 +1,19 @@
 return {
-	'goerz/jupytext.vim',
-	config = function()
-		vim.g.jupytext_fmt = 'py' -- konvertiert .ipynb → .py beim Öffnen
-	end
+	{
+		'goerz/jupytext.vim',
+		config = function()
+			vim.g.jupytext_fmt = 'py' -- konvertiert .ipynb → .py beim Öffnen
+		end
+	},
+	{
+		'benlubas/molten-nvim',
+		version = '^1.0.0',
+		build = ':UpdateRemotePlugins',
+		lazy = false,
+		init = function()
+			vim.g.molten_output_win_max_height = 20
+			vim.g.molten_image_provider = "none"
+			vim.g.python3_host_prog = '/usr/bin/python3'
+		end
+	}
 }
